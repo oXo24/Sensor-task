1f6971dec5506aa077304eac3863ffd6e2ec52fd
pavly <pavlykas0124@gmail.com>
2023-08-31 13:38:30 +0300

    Completed task

---
+++ src/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Substance substance = new Substance();
+
+        new WeightSensor(substance);
+        new ColorSensor(substance);
+
+        System.out.println("Substance temperature +25 ");
+        substance.setState(25);
+
+        System.out.println("Substance temperature -5 ");
+        substance.setState(-5);
+    }
+}
